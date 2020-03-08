0a0e37a14c8899caa8a3551018157860bcd663bc
Juliano Barboza <julianodawid@gmail.com>
2020-03-08 17:50:46 -0300

    removes year, month & start_page attributes

--- remuneracionSpyder.py
+++ remuneracionSpyder.py
@@ -8,6 +8,7 @@ Created on Sat Mar  7 18:13:26 2020
 import scrapy
 from lxml import etree
 from parsel import Selector
+import uuid
 
 class RemuneracionSpider(scrapy.Spider):
     name = 'remuneracion'
@@ -23,17 +24,14 @@ class RemuneracionSpider(scrapy.Spider):
     def parse(self, response):
         for year_link in response.css('a[target=_self]'):
             yield response.follow(year_link.xpath('@href').get(), 
-                                  self.parse_year, 
-                                  cb_kwargs={'year': year_link.xpath('//text()').get()})
+                                  self.parse_year)
 
-    def parse_year(self, response, year='2020'):
+    def parse_year(self, response):
         for month_link in response.css('a[target=_self]'):
             yield response.follow(month_link.xpath('@href').get(), 
-                                  self.parse_month,
-                                  cb_kwargs={'month': month_link.xpath('//text()').get(),
-                                             'year': year})
+                                  self.parse_month)
         
-    def parse_month(self, response, year='2020', month='january'):
+    def parse_month(self, response):
         self.line_titles = response.xpath('//th//text()').getall()
         for line in response.xpath('//tr'):
             if len(line.xpath('td')) > 0:
@@ -56,23 +54,20 @@ class RemuneracionSpider(scrapy.Spider):
                         "A2248:form-visualizar:datosplantilla_encodeFeature": "true"
                 },
                 url=response.css('input[name="javax.faces.encodedURL"]').xpath('@value').get(),
-                callback=self.next_page,
-                cb_kwargs={'year': year,
-                           'month': month,
-                           'start_page': start_page})
+                callback=self.next_page)
         
         
-    def next_page(self, response, year='2020', month='january', start_page=0):
+    def next_page(self, response):
         parser = etree.XMLParser(strip_cdata=False)
         root = etree.fromstring(response.body, parser=parser, base_url=response.url)
         content = Selector(root=root).xpath('//text()').getall()
         inner_xml = content[0]
         root2 = etree.fromstring("<newtable>" + inner_xml + "</newtable>")
-        my_id = year + month + str(start_page)
+        my_id = uuid.uuid4()
         self.selectors[my_id] = Selector(root=root2)
         for i,line in enumerate(self.selectors[my_id].xpath('//tr')):
             if len(line.xpath('td')) > 0:
-                yield {"DEBUG": f"{year} {month} {start_page}, {i}", **dict(zip(self.line_titles, line.xpath('td//text()').getall()))}
+                yield {"DEBUG": f"{i}", **dict(zip(self.line_titles, line.xpath('td//text()').getall()))}
             else:
-                yield {"DEBUG": f"{year} {month} {start_page}, {i}, len td is 0"}
+                yield {"DEBUG": f"{i}, len td is 0"}
         del self.selectors[my_id]
