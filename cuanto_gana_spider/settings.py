1b579b2e883e4b23abf49766fb6e865e09402b27
Juliano Barboza <julianodawid@gmail.com>
2020-03-08 21:23:35 -0300

    configs spider to not obey robots.txt

--- cuanto_gana_spider/settings.py
+++ cuanto_gana_spider/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'cuanto_gana_spider.spiders'
 #USER_AGENT = 'cuanto_gana_spider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
