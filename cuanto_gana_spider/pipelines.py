e6b660e6ed94a6aad13835e51e5c492107ee2831
Juliano Barboza <julianodawid@gmail.com>
2020-03-29 11:53:50 -0300

    adds pipeline to deal with numbers and start/stop args

--- cuanto_gana_spider/pipelines.py
+++ cuanto_gana_spider/pipelines.py
@@ -4,8 +4,22 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from cuanto_gana_spider.resources import month_map
+import re
 
-
-class CuantoGanaSpiderPipeline(object):
+class ProcessNumbers(object):
     def process_item(self, item, spider):
-        return item
+        new_item = item.copy()
+        for key, val in item.items():
+            if(not isinstance(val, str)):
+                continue
+            cleaned_val = re.sub(r'[\.$]', "", val.strip())
+            if(key=="Mes" and val in month_map):
+                number = month_map[val]
+            else:
+                try:
+                    number = int(re.findall(r'\d+', cleaned_val)[0])
+                except:
+                    continue
+            new_item[key + "_number"] = number
+        return new_item
